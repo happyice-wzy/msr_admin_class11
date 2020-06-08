2e85fdc2b046c909f03892fff44649f8514f02c2
wzy <wzyrb1882@gmail.com>
2020-06-08 15:53:05 +0900

    项目完结

--- msr-edu/src/main/java/com/msr/edu/EduApplication.java
+++ msr-edu/src/main/java/com/msr/edu/EduApplication.java
@@ -3,9 +3,12 @@ package com.msr.edu;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 // 主类
 @SpringBootApplication
 @MapperScan("com.msr.edu.mapper")
+@ComponentScan(basePackages={"com.msr.edu","com.msr.common"})
 public class EduApplication {
     public static void main(String[] args) {
         SpringApplication.run(EduApplication.class,args);
